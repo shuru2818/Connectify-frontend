e89ef4fa9ec2346f2120f660e1f30c5b8689874f
Abhishek Verma <abhishekjbverma.com>
2026-05-24 17:34:28 +0530

    Live link changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3200/api",
+  baseURL: "https://connectify-backend-4hav.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3200", {
+const socket = io("https://connectify-backend-4hav.onrender.com", {
   withCredentials: true,
   autoConnect: false,
 });
